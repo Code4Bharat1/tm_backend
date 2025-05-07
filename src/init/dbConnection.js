4174f691caaa0d3daa71f8bee359afc8e5ef3f52
Vaibhav-stack31 <tiwariv1669@gmail.com>
2025-05-07 08:48:47 +0530

    Update dbConnection.js and timesheet controller

--- src/init/dbConnection.js
+++ src/init/dbConnection.js
@@ -1,23 +1,25 @@
-// db.js
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const mongoURI = process.env.MONGO_URI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: 'tm', //database name
-        });
-        console.log('✅ MongoDB connected successfully');
-    } catch (error) {
-        console.error('❌ MongoDB connection error:', error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(mongoURI, {
+      dbName: "tm",
+    });
+    console.log(
+      "✅ MongoDB connected successfully"
+    );
+  } catch (error) {
+    console.error(
+      "❌ MongoDB connection error:",
+      error.message
+    );
+    process.exit(1);
+  }
 };
 
 export default connectDB;
