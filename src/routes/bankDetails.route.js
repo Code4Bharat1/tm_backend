91fdbbf889340277befb280b350347c4af756d69
ParasVarankar23 <parasvarankar235@gmail.com>
2025-05-21 18:20:34 +0530

    changes in bankDetails controller and route

--- src/routes/bankDetails.route.js
+++ src/routes/bankDetails.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { updateUserInfo, getUserInfo } from '../controller/bankDetails.controller.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { updateUserInfo, getUserInfo,getUsersByCompany } from '../controller/bankDetails.controller.js';
+import { protect, protectAdmin } from '../middleware/authMiddleware.js';
 import multer from 'multer';
 
 const upload = multer();
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.post('/bank/updateUserInfo',protect, upload.single('document'), updateUserInfo);
 router.get('/bank/getPersonalInfo',protect, getUserInfo);
+router.get('/bank/getBankDetails',protectAdmin, getUsersByCompany);
 
 export default router;
