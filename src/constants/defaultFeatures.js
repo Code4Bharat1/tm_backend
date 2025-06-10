9edddef2fc1856d8395fac4de8f04d8bb6405238
ParasVarankar23 <parasvarankar235@gmail.com>
2025-06-10 14:32:12 +0530

    feat(defaultFeatures): add 'addteammembers' to Manager's default features

--- src/constants/defaultFeatures.js
+++ src/constants/defaultFeatures.js
@@ -1,6 +1,6 @@
 // src/constants/defaultFeatures.js
 export const defaultFeatures = {
-  Manager: ['viewtimesheet', 'tasks', 'viewattendance', 'viewleave', 'performanceScore'],
+  Manager: ['viewtimesheet', 'tasks', 'viewattendance', 'viewleave', 'performanceScore', 'addteammembers'],
   TeamLeader: ['viewtimesheet', 'teamTasks', 'viewattendance'],
   HR: ['employeeRecords', 'leaveRequests', 'payroll', 'viewexpense', 'addDocument'],
   Salesman:['site-visit']
